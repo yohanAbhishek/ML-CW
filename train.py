e35d2ce2450ec46809bdebeed1e3e9b66ecc9200
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-02-19 22:30:32 +0530

    Added columns to dataset

--- train.py
+++ train.py
@@ -2,9 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+
 
 # Store the dataset into a variable
-data = pd.read_csv('spambase/spambase.data', header=None)
+data = pd.read_csv('C:\\Users\\yohan\\OneDrive\\Desktop\\spam-mail-detection\\spambase\\spambase.data', header=None)
+
+# Specify the dataset columns
+cols = [
+    'word_freq_make', 'word_freq_address', 'word_freq_all', 'word_freq_3d', 'word_freq_our', 'word_freq_over',
+    'word_freq_remove', 'word_freq_internet', 'word_freq_order', 'word_freq_mail', 'word_freq_receive',
+    'word_freq_will', 'word_freq_people', 'word_freq_report', 'word_freq_addresses', 'word_freq_free',
+    'word_freq_business', 'word_freq_email', 'word_freq_you', 'word_freq_credit', 'word_freq_your',
+    'word_freq_font', 'word_freq_000', 'word_freq_money', 'word_freq_hp', 'word_freq_hpl', 'word_freq_george',
+    'word_freq_650', 'word_freq_lab', 'word_freq_labs', 'word_freq_telnet', 'word_freq_857', 'word_freq_data',
+    'word_freq_415', 'word_freq_85', 'word_freq_technology', 'word_freq_1999', 'word_freq_parts', 'word_freq_pm',
+    'word_freq_direct', 'word_freq_cs', 'word_freq_meeting', 'word_freq_original', 'word_freq_project',
+    'word_freq_re', 'word_freq_edu', 'word_freq_table', 'word_freq_conference', 'char_freq_;', 'char_freq_(',
+    'char_freq_[', 'char_freq_!', 'char_freq_$', 'char_freq_#', 'capital_run_length_average',
+    'capital_run_length_longest', 'capital_run_length_total', 'spam'
+]
+
+# Add columns to the dataset
+data.columns = cols
+
+# Shuffle the DataFrame
+data = data.sample(frac=1, random_state=1).reset_index(drop=True)
 
 # print(data.head())
 # print(data.shape)
@@ -20,6 +43,10 @@ y = data.iloc[:, -1]
 
 # view target values
 # print(y[0:5])
+
+# Normalize the feature values
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
